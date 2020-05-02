be448439572f0865cfa8876d8a6066b4e5d9b4dc
Aditya Atluri <AdityaAvinash1@gmail.com>
2020-05-01 21:01:07 -0700

    switch to cpp

---
+++ src/decepticon-1.cpp
@@ -0,0 +1,15 @@
+/**
+ * This file is covered by the LICENSE file in the root of this project.
+ * Copyright 2020 Aditya Atluri <AdityaAvinash1@gmail.com>
+ */
+
+#include <iostream>
+
+#include "decepticon-1.h"
+#include "decepticon-1/decepticon-1.h"
+
+int main(int argc, char *argv[]) {
+  std::cout << "Welcome to project: decepticon-1\n" << std::endl;
+  return 0;
+}
+
